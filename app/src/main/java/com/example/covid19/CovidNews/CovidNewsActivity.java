f4f0a319b9079ba87d3ca3ff09fd08c0209802a8
jainil25 <35983749+jainil25@users.noreply.github.com>
2020-07-03 15:20:40 +1200

    Basic Layout

---
+++ app/src/main/java/com/example/covid19/CovidNews/CovidNewsActivity.java
@@ -0,0 +1,16 @@
+package com.example.covid19.CovidNews;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.covid19.R;
+
+public class CovidNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_covid_news);
+    }
+}
