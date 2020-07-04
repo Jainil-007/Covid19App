8ca14ac4f9fbfd4f1fb0d4440ad39ef01cfa54bc
jainil25 <35983749+jainil25@users.noreply.github.com>
2020-07-04 18:17:09 +1200

    Revert "Merge pull request #2 from Teddiejim/master"
    This reverts commit 5f28208f6338d153f9e8eff5860177e6a282f097, reversing
    changes made to f4f0a319b9079ba87d3ca3ff09fd08c0209802a8.

--- app/src/main/java/com/example/covid19/Home_ScreenActivity.kt
+++ app/src/main/java/com/example/covid19/Home_ScreenActivity.kt
@@ -1,13 +1,11 @@
 package com.example.covid19
 
-import android.Manifest
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
-import android.view.View
-import android.widget.*
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import com.example.covid19.AffectedCountries.AffectedCountries
@@ -20,10 +18,10 @@ class Home_ScreenActivity : AppCompatActivity() {
     val phoneNumber = "0800 611 116"
     val REQUEST_PHONE_CALL = 1
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_screen)
-
         init()
     }
 
@@ -44,44 +42,15 @@ class Home_ScreenActivity : AppCompatActivity() {
             //method call for email intent with these inputs as parameters
             sendEmail(recipient, subject, message)
         }
-        val send = Intent(this, SettingActivity::class.java)
 
-        val spinnerItem = resources.getStringArray(R.array.menu)
-        val spinner = findViewById<Spinner>(R.id.menu_Spinner)
-        if (spinner != null){
-            val adapter = ArrayAdapter(
-                this,
-                android.R.layout.simple_spinner_item,
-                spinnerItem
-            )
-            adapter.setDropDownViewResource(R.layout.spin)
-            spinner.adapter = adapter
 
-            spinner.onItemSelectedListener = object :
-                AdapterView.OnItemSelectedListener{
-                override fun onNothingSelected(parent: AdapterView<*>?) {
-                    TODO("Not yet implemented")
-                }
 
-                override fun onItemSelected(
-                    parent: AdapterView<*>?,
-                    view: View?,
-                    position: Int,
-                    id: Long
-                ) {
-                    if(spinnerItem[position] == "Change Details"){
-                        val user = intent.getStringExtra("Username")
-                        send.putExtra("Username", user)
-                        startActivity(send)
-                    }
-                }
-            }
 
-        }
+
 
        button.setOnClickListener {
-           if(ActivityCompat.checkSelfPermission(this, Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
-               ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
+           if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED){
+               ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.CALL_PHONE),REQUEST_PHONE_CALL)
            }else{
                startCall()
            }
